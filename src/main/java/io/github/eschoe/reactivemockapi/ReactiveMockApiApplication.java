0925993e29be6a2bf2a569ddb28aaa2afee75805
ChoeEuiSeung <88296182+eschoeDeveloper@users.noreply.github.com>
2025-07-30 22:30:25 +0900

    프로젝트 생성 후 커밋

---
+++ src/main/java/io/github/eschoe/reactivemockapi/ReactiveMockApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.eschoe.reactivemockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMockApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveMockApiApplication.class, args);
+    }
+
+}
