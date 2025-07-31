4c7a51a2c20814c95004f833c57d97e468876271
ChoeEuiSeung <88296182+eschoeDeveloper@users.noreply.github.com>
2025-07-31 13:54:34 +0900

    feat(config: WebFlux Web Config 생성

--- src/main/java/io/github/eschoe/reactivemockapi/config/ReactiveWebConfig.java
+++ src/main/java/io/github/eschoe/reactivemockapi/config/ReactiveWebConfig.java
@@ -1,8 +1,38 @@
 package io.github.eschoe.reactivemockapi.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.PathMatchConfigurer;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
-public class ReactiveWebConfig {
+@EnableWebFlux
+public class ReactiveWebConfig implements WebFluxConfigurer {
 
+    /**
+     * Resource 경로 및 Cache 지정
+     * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/public", "classpath:/static")
+                .setCacheControl(CacheControl.maxAge(20, TimeUnit.SECONDS));
+
+    }
+
+    /**
+     * 모든 요청에서 전달되는 /api/xxx에 대하여 RestController에 기본 path인 /api를 지정하는 메서드
+    * */
+    @Override
+    public void configurePathMatching(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController.class));
+    }
+    
 }
