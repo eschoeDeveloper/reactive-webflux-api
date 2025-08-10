436d1cc2c777dae38db53a84fd01132b69e6db95
ChoeEuiSeung <88296182+eschoeDeveloper@users.noreply.github.com>
2025-08-10 22:04:11 +0900

    feat(auth): logout api 구성

--- src/main/java/io/github/eschoe/reactivemockapi/controller/AuthController.java
+++ src/main/java/io/github/eschoe/reactivemockapi/controller/AuthController.java
@@ -1,8 +1,10 @@
 package io.github.eschoe.reactivemockapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -13,6 +15,22 @@ public class AuthController {
     public Mono<Void> login(String username, String password) {
 
         return Mono.empty();
+
+    }
+
+    @PostMapping("/logout")
+    public Mono<Void> logout(ServerWebExchange exchange) {
+
+        /***
+         * Mono.fromRunnable()을 사용하여 비동기 작업을 정의하고, exchange.getSession()을 통해 세션을 가져와서 invalidate()를 호출하여 세션을 무효화합니다.
+         * 세션 종료 처리를 doOnTerminate()를 통해 지정하고, 세션 무효화 작업이 끝날 때까지 기다리도록 합니다.
+         */
+        return Mono.fromRunnable(() -> {
+            exchange.getSession().doOnTerminate(() -> {
+                exchange.getSession().flatMap(webSession -> webSession.invalidate());
+            });
+        });
+
 
     }
 
